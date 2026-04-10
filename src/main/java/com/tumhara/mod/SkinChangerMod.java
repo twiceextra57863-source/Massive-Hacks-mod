d08ea0b69bbc225aae143df0a8c6e7c4bcd79a56
twiceextra57863-source <twiceextra57863@gmail.com>
2026-04-10 14:15:47 +0530

    Update SkinChangerMod.java

--- src/main/java/com/tumhara/mod/SkinChangerMod.java
+++ src/main/java/com/tumhara/mod/SkinChangerMod.java
@@ -3,13 +3,20 @@ package com.tumhara.mod;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.io.File;
 
 public class SkinChangerMod implements ModInitializer {
     public static final String MOD_ID = "skinchanger";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final File CONFIG_DIR = new File("config/skinchanger");
 
     @Override
     public void onInitialize() {
-        LOGGER.info("Skin Changer Mod initialized for 1.21.4!");
+        if (!CONFIG_DIR.exists()) {
+            CONFIG_DIR.mkdirs();
+            LOGGER.info("Created config directory: " + CONFIG_DIR.getAbsolutePath());
+        }
+        LOGGER.info("§a✓ Skin Changer Mod initialized for 1.21.4!");
+        LOGGER.info("§e📁 Config folder: " + CONFIG_DIR.getAbsolutePath());
     }
 }
