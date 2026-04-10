5db16b5cea316f638c13b0fe110309e36160dfb0
twiceextra57863-source <twiceextra57863@gmail.com>
2026-04-10 21:02:05 +0530

    Update SkinDashboardMod.java

--- src/main/java/com/yourname/skincustomizer/SkinDashboardMod.java
+++ src/main/java/com/yourname/skincustomizer/SkinDashboardMod.java
@@ -1,10 +1,15 @@
 package com.yourname.skincustomizer;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
+@Environment(EnvType.CLIENT)
 public class SkinDashboardMod implements ClientModInitializer {
+    public static final String MOD_ID = "skincustomizer";
+    
     @Override
     public void onInitializeClient() {
-        // Client-side initialization (agar baad me config/events chahiye ho)
+        // Client-side init - agar baad me config/events chahiye ho
     }
 }
