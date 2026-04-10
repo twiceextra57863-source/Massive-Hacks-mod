f569daae95b7f6309da8ef273220e8605df4f30e
twiceextra57863-source <twiceextra57863@gmail.com>
2026-04-10 20:53:25 +0530

    Create SkinDashboardMod.java

---
+++ src/main/java/com/yourname/skincustomizer/SkinDashboardMod.java
@@ -0,0 +1,10 @@
+package com.yourname.skincustomizer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkinDashboardMod implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Client-side initialization (agar baad me config/events chahiye ho)
+    }
+}
