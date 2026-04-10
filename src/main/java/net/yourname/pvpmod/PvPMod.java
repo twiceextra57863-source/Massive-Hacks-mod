7da2aa9ca93869b951463f534d4d72b354e593a9
twiceextra57863-source <twiceextra57863@gmail.com>
2026-04-10 21:32:05 +0530

    Create PvPMod.java

---
+++ src/main/java/net/yourname/pvpmod/PvPMod.java
@@ -0,0 +1,12 @@
+package net.yourname.pvpmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PvPMod implements ModInitializer {
+    public static final String MOD_ID = "pvpmod";
+
+    @Override
+    public void onInitialize() {
+        // Server-side ya shared logic yahan aayega (abhi khali)
+    }
+}
